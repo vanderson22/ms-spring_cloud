0608bf2b763c5b6ac77c0c9fba31e349e79d0694
Vanderson Nogueira <vanderson.01@hotmail.com>
2021-01-14 10:11:43 -0300

    hr-eureka server inicial

---
+++ hr-eureka-server/src/test/java/com/home/ms/eureka/HrEurekaServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.home.ms.eureka;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HrEurekaServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
