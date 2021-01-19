a15f5b11b3eb4e1d9c12b568a14ba28fccfd0b56
Vanderson Nogueira <vanderson.01@hotmail.com>
2021-01-19 16:49:08 -0300

    random port

--- hr-payroll/src/main/java/com/home/ms/HrPayrollApplication.java
+++ hr-payroll/src/main/java/com/home/ms/HrPayrollApplication.java
@@ -2,13 +2,12 @@ package com.home.ms;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.netflix.ribbon.RibbonClient;
+import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
-// informar o nome do serviço no qual é cliente - consome hr-worker
-@RibbonClient(name="hr-worker")
-// suporte ao feign,
-	// integração com spring cloud e limpeza de código
+@EnableCircuitBreaker
+@EnableEurekaClient
 @EnableFeignClients
 @SpringBootApplication
 public class HrPayrollApplication {
