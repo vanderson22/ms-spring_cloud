1f6ddd045ab5db8d641202e7fed742ff321bdf5b
Vanderson Nogueira <vanderson.01@hotmail.com>
2021-01-29 13:31:25 -0300

    [zull] Roteamento com Zull api Gateway

--- hr-api-gateway-zull/src/main/java/com/home/ms/api/HrApiGatewayZullApplication.java
+++ hr-api-gateway-zull/src/main/java/com/home/ms/api/HrApiGatewayZullApplication.java
@@ -2,7 +2,12 @@ package com.home.ms.api;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 
+
+@EnableEurekaClient
+@EnableZuulProxy
 @SpringBootApplication
 public class HrApiGatewayZullApplication {
 
