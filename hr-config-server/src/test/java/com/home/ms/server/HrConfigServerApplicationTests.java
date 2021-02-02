3b4344bf674813767540b03ce384bf4b310880cd
Vanderson Nogueira <vanderson.01@hotmail.com>
2021-02-02 10:18:34 -0300

    [config-server] Inicial config server

---
+++ hr-config-server/src/test/java/com/home/ms/server/HrConfigServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.home.ms.server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HrConfigServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
