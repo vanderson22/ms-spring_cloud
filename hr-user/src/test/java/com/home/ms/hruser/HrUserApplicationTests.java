9610a7c03f6813b8a94a282ef35316ef0cf12b63
vanderson nogueira <vanderson.01@hotmail.com>
2021-12-01 19:27:53 -0300

    [hr-user] configuring hr-user

---
+++ hr-user/src/test/java/com/home/ms/hruser/HrUserApplicationTests.java
@@ -0,0 +1,13 @@
+package com.home.ms.hruser;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HrUserApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
