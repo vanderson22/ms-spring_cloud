9f7b38531001f8459bd3fba0363a7fa7c898e1dd
vanderson nogueira <vanderson.01@hotmail.com>
2021-12-09 17:55:06 -0300

    [zull] Cors configuration

--- hr-api-gateway-zull/src/main/java/com/home/ms/api/config/ResourceServerConfig.java
+++ hr-api-gateway-zull/src/main/java/com/home/ms/api/config/ResourceServerConfig.java
@@ -1,13 +1,22 @@
 package com.home.ms.api.config;
 
+import java.util.Arrays;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.Ordered;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
 import org.springframework.security.oauth2.config.annotation.web.configuration.ResourceServerConfigurerAdapter;
 import org.springframework.security.oauth2.config.annotation.web.configurers.ResourceServerSecurityConfigurer;
 import org.springframework.security.oauth2.provider.token.store.JwtTokenStore;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
 
 @EnableResourceServer
 @Configuration
@@ -35,7 +44,7 @@ public class ResourceServerConfig extends ResourceServerConfigurerAdapter {
 		
 //		 http.authorizeRequests()
 //	 	 .antMatchers("/**").permitAll();
-//	 	 
+ 	 	 http.cors().configurationSource(corsConfigurationSource());
 		 http.authorizeRequests()
 		 	 .antMatchers(PUBLIC).permitAll()
 		 	 .antMatchers(HttpMethod.GET , OPERATOR).hasAnyRole(OPERATOR_ROLE , ADMIN_ROLE)
@@ -48,6 +57,32 @@ public class ResourceServerConfig extends ResourceServerConfigurerAdapter {
 	@Override
 	public void configure(ResourceServerSecurityConfigurer resources) throws Exception {
 		resources.tokenStore(tokenStore);
+	}
+	
+	@Bean
+	public CorsConfigurationSource corsConfigurationSource()	{
+	    CorsConfiguration corsConfiguration = new CorsConfiguration();
+	    //qualquer origem
+	    corsConfiguration.setAllowedOrigins(Arrays.asList("*"));
+	    corsConfiguration.setAllowedMethods(Arrays.asList("*"));
+	    corsConfiguration.setAllowCredentials(Boolean.TRUE);
+	    corsConfiguration.setAllowedMethods(Arrays.asList("Authorization" , "Content-Type"));
+	
+	    UrlBasedCorsConfigurationSource urlBasedCorsConfigurationSource = new UrlBasedCorsConfigurationSource();
+	    
+	    urlBasedCorsConfigurationSource.registerCorsConfiguration("/**", corsConfiguration);
+	
+	  return urlBasedCorsConfigurationSource;
+	}
+	
+	@Bean
+	public FilterRegistrationBean<CorsFilter> corsFilter()
+	{
+		   FilterRegistrationBean<CorsFilter> bean = new FilterRegistrationBean<>( new CorsFilter(corsConfigurationSource()));
+		   
+		   bean.setOrder(Ordered.HIGHEST_PRECEDENCE);
+		   
+		   	return bean;
 	}
 
 }
